5794c76ffe1f74a2c35516a165d4d1043aaf79fa
Rajan Agarwal <rajan.ag005@gmail.com>
2021-12-03 16:28:58 -0500

    initial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="App">
         
         <section className="main">
-      <h1><i>purple coconuts</i></h1>
+      <h1><i>empower.</i></h1>
       </section>
       <section className="section">
       <h3>I'm Rajan—a 16 year old hacktivist based in Toronto. I hack, I cry but most importantly, I despise. I am a firm believer that the lack of transparency is synonymous to an inchoate digital presence. In your exploration of my digital persona, I hope that you develop a profound understanding of my world, built on the foundation of creation and change.
