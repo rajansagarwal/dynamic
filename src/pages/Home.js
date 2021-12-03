e44c57cd4c95c3c35e6027a42246e8364b5ab0e7
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-12-03 21:14:45 +0000

    updoot

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+			
+
+export default function Home() {
+    return (
+    <div className="App">
+        
+        <section className="main">
+      <h1><i>purple coconuts</i></h1>
+      </section>
+      <section className="section">
+      <h3>I'm Rajan—a 16 year old hacktivist based in Toronto. I hack, I cry but most importantly, I despise. I am a firm believer that the lack of transparency is synonymous to an inchoate digital presence. In your exploration of my digital persona, I hope that you develop a profound understanding of my world, built on the foundation of creation and change.
+
+</h3>
+
+      </section>
+
+    </div>
+    );
+}
