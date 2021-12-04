7cc8a7791c0cab96952973f1b9b5272d7be816c4
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-12-04 16:26:51 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "../App.css";
+import Transition from '../components/Transition';
 			
 
 export default function Home() {
     return (
-    <div className="App">
+    <div className="App"> 
       
         <section className="main">
       
-      <h1><i><span>think.</span> <span>build.</span> <span>empower.</span> </i></h1>
+      <div id="inline"><h1><i><span><Transition/></span> </i></h1></div>
       </section> 
       <section className="section">
       <h2><i>I'm Rajan</i>—a 16 year old hacktivist & author based in Toronto. I build, write and redefine. <br/><br/>
       I work with changemakers to grow organizations, ideas, projects and initiatives.<br/><br/>
-      My philosophy.</h2>
+      my philosophy; transparency is symbolic of digital presence.</h2>
+      <h3>
+
+      </h3> 
+
+
+      </section>
+      <section className="contact">
+      <h2><i>I'm Rajan</i>—a 16 year old hacktivist & author based in Toronto. I build, write and redefine. <br/><br/>
+      I work with changemakers to grow organizations, ideas, projects and initiatives.<br/><br/>
+      my philosophy; transparency is symbolic of digital presence.</h2>
       <h3>
 
       </h3> 
