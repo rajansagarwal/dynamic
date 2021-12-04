f3a8e46423e7ac69eb481272e6c1e6f845fc95d9
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-12-04 09:39:07 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,19 @@ import "../App.css";
 export default function Home() {
     return (
     <div className="App">
-        
+      
         <section className="main">
-      <h1><i>think. build. empower. </i></h1>
-      </section>
+      
+      <h1><i><span>think.</span> <span>build.</span> <span>empower.</span> </i></h1>
+      </section> 
       <section className="section">
-      <h2><i>I'm Rajan—a 16 year old hacktivist & author based in Toronto. I build, write and redefine. <br/><br/>
+      <h2><i>I'm Rajan</i>—a 16 year old hacktivist & author based in Toronto. I build, write and redefine. <br/><br/>
       I work with changemakers to grow organizations, ideas, projects and initiatives.<br/><br/>
-      My philosophy.</i></h2>
-
-      </section>
-      <section className="section">
+      My philosophy.</h2>
       <h3>
 
-</h3>
+      </h3> 
+
 
       </section>
 
