c1406d1bdad7ebe3b601e46ea704240f16dc93d5
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-12-04 09:39:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
+     
       <header className="App-header">
         <Home/>
       </header>
+      
     </div>
   );
 }
